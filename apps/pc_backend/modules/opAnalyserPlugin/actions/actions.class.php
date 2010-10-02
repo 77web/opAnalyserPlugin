89daba3427a5d5cf044d6eecc5e89aca06d0802e
77web <info@77-web.com>
2010-10-02 23:30:52 +0900

    update action, templates for pc_backend module opAnalyserPlugin

--- apps/pc_backend/modules/opAnalyserPlugin/actions/actions.class.php
+++ apps/pc_backend/modules/opAnalyserPlugin/actions/actions.class.php
@@ -1,5 +1,34 @@
 <?php
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * opAnalyserPlugin actions.
+ *
+ * @package    OpenPNE
+ * @subpackage opAnalyserPlugin
+ * @author     Hiromi Hishida <info@77-web.com>
+ */
 
 class opAnalyserPluginActions extends sfActions
 {
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->form = new opAnalyserPluginConfigForm();
+    if ($request->isMethod(sfWebRequest::POST))
+    {
+      $this->form->bind($request->getParameter('analyser_plugin_config'));
+      if ($this->form->isValid())
+      {
+        $this->form->save();
+        $this->redirect('opAnalyserPlugin/index');
+      }
+    }
+
+  }
 }
